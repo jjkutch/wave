a45493b1d71956100270d6d048ba6d2243ac2869
jjkutch <115191922+jjkutch@users.noreply.github.com>
2022-10-06 21:46:44 -0700

    Board added

--- TsunamiWave/Assets/_Tutorial/BlendShapeScript.cs
+++ TsunamiWave/Assets/_Tutorial/BlendShapeScript.cs
@@ -6,27 +6,31 @@ public class BlendShapeScript : MonoBehaviour
 {
 
 	SkinnedMeshRenderer skinnedMeshRenderer;
-    	Mesh skinnedMesh;
+    Mesh skinnedMesh;
+    Vector3[] vertices;
 	int blendShapeCount;
 	int playIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-	skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer> ();
+	    skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer> ();
         skinnedMesh = GetComponent<SkinnedMeshRenderer>().sharedMesh;
-	blendShapeCount = skinnedMesh.blendShapeCount; 
+	    blendShapeCount = skinnedMesh.blendShapeCount; 
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playIndex > 0) skinnedMeshRenderer.SetBlendShapeWeight(playIndex-1,0f);
-	if(playIndex == 0) skinnedMeshRenderer.SetBlendShapeWeight(blendShapeCount-1,0f);
 
-	skinnedMeshRenderer.SetBlendShapeWeight(playIndex, 100f);
-	playIndex++;
-	if(playIndex > blendShapeCount-1) playIndex = 0;  
+        //vertices = skinnedMesh.vertices;
+
+        if(playIndex > 0) skinnedMeshRenderer.SetBlendShapeWeight(playIndex-1,0f);
+	    if(playIndex == 0) skinnedMeshRenderer.SetBlendShapeWeight(blendShapeCount-1,0f);
+
+	    skinnedMeshRenderer.SetBlendShapeWeight(playIndex, 100f);
+	    playIndex++;
+	    if(playIndex > blendShapeCount-1) playIndex = 0;  
     }
 }
